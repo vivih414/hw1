ccae3c1459dd00fabdf8d162628aa15dcb5c28af
Viviana Herrera <vh89445@usc.edu>
2025-01-29 01:54:29 +0000

    completed part 1

--- split.cpp
+++ split.cpp
@@ -18,6 +18,26 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if(in == nullptr)//Base Case
+  {
+    return;
+  }
+
+  Node* curr = in;
+  in = in -> next;
+
+  if(curr -> value % 2 == 0)//check if next val is even
+  {
+    curr -> next = evens;
+    evens = curr;
+  }
+  else//if not even then odd
+  {
+    curr -> next = odds;
+    odds = curr;
+  }
+
+  split(in, odds, evens);//use recursion for rest of list
 }
 
 /* If you needed a helper function, write it here */
