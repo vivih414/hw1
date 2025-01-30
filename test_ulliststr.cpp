9b19b39f0ce2b1116e8989cf79cd698f5a565b51
Viviana Herrera <vh89445@usc.edu>
2025-01-30 01:55:14 +0000

    completed part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,28 @@
 
 int main(int argc, char* argv[])
 {
+  ULListStr list;
 
+  list.push_back("V");
+  std::cout << "Front: " << list.front() << " Back: "
+  << list.back() << " Size: " << list.size() << std::endl;
+
+  list.push_front("I");
+  std::cout << "Front: " << list.front() << " Back: "
+  << list.back() << " Size: " << list.size() << std::endl;
+
+  list.pop_back();
+  std::cout << "Back: " << list.back() << " Size: " << list.size() << std::endl;
+
+  list.pop_front();
+  std::cout << "Empty list " << list.empty() << std::endl;
+
+  list.push_back("H");
+  list.set(0, "E");
+  std::cout << "Val at 0 " << list.get(0) << std::endl;
+
+  list.clear();
+  std::cout << "Empty list " << list.empty() << std::endl;
+
+  return 0;
 }
